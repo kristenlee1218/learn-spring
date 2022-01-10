5b21e6e2318593035acb86643eca7e0ce4365b7f
kristen <1065200748@qq.com>
2022-01-11 00:19:04 +0800

    2022.1.11

--- lesson3/src/main/java/com/learn/spring/DemoTest.java
+++ lesson3/src/main/java/com/learn/spring/DemoTest.java
@@ -1,5 +1,6 @@
 package com.learn.spring;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,7 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class DemoTest {
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        context.close();
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        System.out.println(context.getBean("user1"));
+        System.out.println(context.getBean("user2"));
     }
 }
