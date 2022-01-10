bb869cb7df41a6a1bc68dff130d123a498d4d2c2
kristen <1065200748@qq.com>
2022-01-11 00:09:51 +0800

    2022.1.11

---
+++ lesson4/src/main/java/com/learn/spring/bean/User.java
@@ -0,0 +1,10 @@
+package com.learn.spring.bean;
+
+/**
+ * @author kristen
+ */
+public class User {
+    public User() {
+        System.out.println("我被 spring 实例化了");
+    }
+}
---
+++ lesson4/src/main/java/com/learn/spring/factory/BeanFactory.java
@@ -0,0 +1,16 @@
+package com.learn.spring.factory;
+
+import com.learn.spring.bean.User;
+
+public class BeanFactory {
+    private static User user1 = new User();
+    private static User user2 = new User();
+
+    public User createUser1() {
+        return user1;
+    }
+
+    public User createUser2() {
+        return user2;
+    }
+}
