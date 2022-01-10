91cb2f27909621f3ae0f75ccada421c35a32f669
kristen <1065200748@qq.com>
2022-01-11 00:11:48 +0800

    2022.1.11

--- lesson4/src/main/java/com/learn/spring/factory/BeanFactory.java
+++ lesson4/src/main/java/com/learn/spring/factory/BeanFactory.java
@@ -2,6 +2,9 @@ package com.learn.spring.factory;
 
 import com.learn.spring.bean.User;
 
+/**
+ * @author kristen
+ */
 public class BeanFactory {
     private static User user1 = new User();
     private static User user2 = new User();
