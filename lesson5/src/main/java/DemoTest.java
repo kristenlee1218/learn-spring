2590108a062be6e29016ae6a5db67c9ce53aff18
kristen <1065200748@qq.com>
2022-01-13 17:03:47 +0800

    2022.1.13

--- lesson5/src/main/java/DemoTest.java
+++ lesson5/src/main/java/DemoTest.java
@@ -1,14 +1,12 @@
-import com.learn.spring.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * @author kristen
+ * @author Kristen
  */
 public class DemoTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        UserService service = (UserService) context.getBean("userService");
-        service.saveUser();
+        System.out.println(context.getBean("user"));
     }
 }
